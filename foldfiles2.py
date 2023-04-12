73cce70afe2ed5c4f18edc491b5703532e635a89
Mourya <mouryag99@gmail.com>
2023-04-12 16:16:57 +0530

    into feat2

--- foldfiles2.py
+++ foldfiles2.py
@@ -2,14 +2,14 @@ import os
 from PyPDF2 import PdfFileReader
 import pandas as pd
 
-folder_path = '/home/xpms/Downloads/OneDrive_2_28-02-2023'
+folder_path = '/home/3'
 folders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]
 files_info = []
 
 print(folders)
 for folder in folders:
     f_path = os.path.join(folder_path, folder)
-    for file_name in os.listdir(f_path):
+    for file_name in os.listdir(path):
         files_info.append(file_name)
 df = pd.DataFrame(files_info)
 newdf = df.drop_duplicates()
