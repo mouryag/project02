2e7222a2196acdc6d99888d7d9c854b46c52a7c4
Mourya <mouryag99@gmail.com>
2023-04-12 16:27:26 +0530

    feat2 3

--- foldfiles2.py
+++ foldfiles2.py
@@ -2,17 +2,15 @@ import os
 from PyPDF2 import PdfFileReader
 import pandas as pd
 
-folder_path = '/home/3'
+folder_path = '/home/xpms/Downloads/OneDrive_2_28-02-2023'
 folders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]
 files_info = []
 
 print(folders)
 for folder in folders:
     f_path = os.path.join(folder_path, folder)
-    for file_name in os.listdir(path):
+    for file_name in os.listdir(f_path):
         files_info.append(file_name)
 df = pd.DataFrame(files_info)
 newdf = df.drop_duplicates()
 newdf.to_csv("contracts_files72.csv", index=False)
-print("sums up my format below")
-print("gibberish above")
